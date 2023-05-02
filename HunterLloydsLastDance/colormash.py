c41b8e4560f6b150a1dd173c082fbf9b99f11b06
SeaTurtle22 <joey@grayturtle.co>
2023-05-01 19:43:06 -0600

    ahhhhhh

--- HunterLloydsLastDance/colormash.py
+++ HunterLloydsLastDance/colormash.py
@@ -52,7 +52,7 @@ try:
 
         # Convert images to numpy arrays
         color_image = np.asanyarray(color_frame.get_data())
-        color_image = cv2.convertScaleAbs(color_image, alpha=5, beta=-50)
+        color_image = cv2.convertScaleAbs(color_image, alpha=5, beta=-127)
         color_image = cv2.normalize(color_image, None, alpha=0, beta=200, norm_type=cv2.NORM_MINMAX)
         hsv_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2HSV)
 
