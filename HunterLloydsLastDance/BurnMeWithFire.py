11a609a84c486bbf55f08d5dc7d528929deb4508
SeaTurtle22 <joey@grayturtle.co>
2023-05-01 20:26:44 -0600

    ahhhhhh

--- HunterLloydsLastDance/BurnMeWithFire.py
+++ HunterLloydsLastDance/BurnMeWithFire.py
@@ -139,13 +139,7 @@ class StateMachine:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Detect blobs.
-        keypoints = detector.detect(gray)
 
-
-        # Draw detected blobs as red circles.
-        # cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
-        frame = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 0, 255),
-                                              cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         # Show keypoints
         cv2.namedWindow(window, cv2.WINDOW_AUTOSIZE)
